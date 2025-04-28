47bbb2c5023e39c7cbbaca3e16ad91f2458ae096
ferdikendir <ferdikendir@gmail.com>
2025-04-28 14:28:55 +0300

    modified dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, computed } from "@angular/core";
 import { todos } from "@core/store";
 import { SharedModule } from "@shared/shared.module";
+import { TodoFormComponent } from "../todo/todo-form/todo-form.component";
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
     imports: [
-        SharedModule
+        SharedModule,
+        TodoFormComponent,
     ]
 })
 export class DashboardComponent {
-    completedTodos = todos().filter(todo => todo.completed);
-    allTodos = todos();
-    pendingTodos = todos().filter(todo => !todo.completed);
+
+    completedTodos = computed(() => todos().filter(todo => todo.completed));
+
+    allTodos = computed(() => todos());
+    pendingTodos = computed(() => todos().filter(todo => !todo.completed));
 }
